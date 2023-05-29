c1df8e3562828e3570900304836dee3e587277eb
Susan Minott <susan_minott@yahoo.com>
2023-05-29 14:45:09 -0400

    Find Single Movie by ObjectId

---
+++ src/main/java/dev/susanm/movies/Movie.java
@@ -0,0 +1,31 @@
+package dev.susanm.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document (collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbID;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+//    DocRef: Stores only the IDs of the reviews & the actual reviews will be in a separate collection.
+    private List<Review> reviewIds;
+}
